0e66b6134f48a6c510bdc757057c2462c8308bee
Alex Murphy <lordpizza11@gmail.com>
2020-05-14 22:13:01 +0200

    Added image component and images to app component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import giraffe from './images/giraffe1.jpg'
+import notGiraffe from './images/notgiraffe1.jpg'
+
+class Image extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <img src={this.props.image}></img>
+    );
+  }
+
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <Image image={giraffe} />
+        <Image image={notGiraffe} />
+      </div>
+    );
+  }
+
+}
 
 ReactDOM.render(
-  
+  <App />,
   document.getElementById('root')
 );
