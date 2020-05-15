b88e1bcb9394961d0d119126dc68eabb760d6017
Alex Murphy <lordpizza11@gmail.com>
2020-05-15 18:18:21 +0200

    Improved Counter

--- src/index.js
+++ src/index.js
@@ -4,6 +4,33 @@ import './index.scss';
 import giraffe from './images/giraffe1.jpg'
 import notGiraffe from './images/notgiraffe1.jpg'
 
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {count: 0};
+  }
+
+  changeCounter(reset) {
+    console.log(reset);
+    if (!reset) {
+      this.setState((state) => ({
+        count: state.count + 1
+      }));
+    } else {
+      this.setState({
+        count: 0
+      });
+    }
+  }
+
+  render(){
+    return(
+      <p>{this.state.count}</p>
+    );
+  }
+
+}
+
 class Button extends React.Component {
   constructor(props) {
     super(props);
@@ -11,7 +38,7 @@ class Button extends React.Component {
 
   render() {
     return (
-    <button>{this.props.name}</button>
+    <button onClick={this.props.onClick}>{this.props.name}</button>
     );
   }
 
@@ -23,8 +50,9 @@ class Image extends React.Component {
   }
 
   render() {
+    const alt = "This is an image of either a giraffe or not a giraffe";
     return (
-      <img src={this.props.image}></img>
+      <img src={this.props.image} alt={alt}></img>
     );
   }
 
@@ -33,18 +61,41 @@ class Image extends React.Component {
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {count:0};
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+    this.counterElement = React.createRef();
+  }
+
+  handleButtonClick(shouldBeGiraffe) {
+    this.counterElement.current.changeCounter(!shouldBeGiraffe);
+    this.changeCounter(!shouldBeGiraffe);
+  }
+
+  changeCounter(reset) {
+    if (!reset) {
+      this.setState((state) => ({
+        count: state.count + 1
+      }));
+    } else {
+      this.setState((state) => ({
+        count: 0
+      }));
+    }
   }
 
   render() {
     return (
       <div>
         <div>
-          <Image image={giraffe}/>
-          <Image image={notGiraffe}/>
+          <Image image={giraffe} isGiraffe={true}/>
+          <Image image={notGiraffe} isGiraffe={false}/>
         </div>
-        <Button name="Left"/>
-        <Button name="No Giraffes"/>
-        <Button name="Right"/>
+        <div>
+          <Button name="Left" onClick={() => this.handleButtonClick(true)}/>
+          <Button name="No Giraffes"/>
+          <Button name="Right" onClick={() => this.handleButtonClick(false)}/>
+        </div>
+        <Counter ref={this.counterElement}/>
       </div>
     );
   }
